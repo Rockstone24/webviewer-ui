f3fa5a374a0a5fb944e711a4730f940e82559182
Philipp Schauhuber <p.schauhuber24@gmail.com>
2020-12-10 17:47:32 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -178,13 +178,15 @@ if (window.CanvasRenderingContext2D) {
   const queryString = window.location.href;
   let versionNumb = parseInt(queryString.substr(-1));
   
-  if(versionNumb === 1){
+  if(versionNumb === 1) {
     disableElements(store)([
       'toolbarGroup-Insert',
       'toolbarGroup-Edit',
       'toolbarGroup-View',
       'toolbarGroup-Shapes',
     ]);
+
+  }
 
     if(versionNumb === 2){
       disableElements(store)([
